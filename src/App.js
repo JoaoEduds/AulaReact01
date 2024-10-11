1d3d9ee74c748b7c42a3088643acd6783b9e0339
JoaoEduds <joaoeduardocaldasmedeiros@gmail.com>
2024-10-11 09:43:18 -0300

    Gerenciando um ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Data from "./meusComponentes/Data";
 import Cont from "./meusComponentes/Contv2";
+import DataV2 from "./meusComponentes/DataV2";
+
 function App() {
   return (
     <div className="App">
         <Data texto="Data e hora atuais: " timeZone={-3.00}/>
         <Data texto="Data e hora em Londres: " timeZone={1.00}/>
         <Data texto="Data e hora em Nova York:" timeZone={-4.00}/>
+        <DataV2 texto="Data JSX " timeZone={-3.00}/>
         <Cont/>
     </div>
   );
---
+++ src/meusComponentes/DataV2.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+/*useEffect é um hook que permite gerenciar o ciclo de vida do componente quando ele é desenvolvido 
+utilizando a sintaxe JSX
+
+a sintaxe do useEffect
+useEffect( function (){
+
+    return function () <-- willUnmount
+}, [] didMont //executa uma vez só)
+   [lista de valores observados] didUpdate para cada valor que foi
+*/
+
+export default function DataV2(props){
+    
+    const [dataAtual, setDataAtual] = useState("");
+
+    function pegaDataDe(timeZone){
+        const dataAtual = new Date();
+        let timeZoneFromDB = parseInt(timeZone); 
+        let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset();
+        let milisegundos = parseInt(dataAtual.getTime() + (diferencaTempo * 60 * 1000));
+        const data = new Date(milisegundos);
+        return data;
+    }
+
+    //exemplo de didMount
+    useEffect(()=>{
+        setDataAtual(new Date().toLocaleString());
+        return ()=>{} //willUnmount
+    }, [])
+
+    //exemplo de didUpdate
+    useEffect(()=>{
+        setTimeout(()=>{
+                setDataAtual(pegaDataDe(props.timeZone).toLocaleString()); 
+            },1000);
+    },[dataAtual]) //<-- O que observar para executar a função a cada atualização da data
+    
+    return(
+        <h1>{ props.texto || ""}{dataAtual}</h1>
+    );
+}
